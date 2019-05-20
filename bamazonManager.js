6a49cff541b09f5a619b12e15b78987233909e05
Donna Kuang <dolaku@gmail.com>
2019-05-20 16:18:47 -0400

    Created switch statement for actions

--- bamazonManager.js
+++ bamazonManager.js
@@ -11,9 +11,43 @@ const connection = mysql.createConnection({
 });
 
 
-connection.connect((err) => {
+connection.connect( (err) => {
     if (err) throw err;
     // console.log("connected as id " + connection.threadId + "\n");
+
+    inquirer
+    .prompt([
+        {
+            type: 'list',
+            name: 'action',
+            message: 'What would you like to do?'.green,
+            choices: [
+                'View Products for Sale',
+                'View Low Inventory',
+                'Add to Inventory',
+                'Add New Product',
+                'Nothing, exit'
+            ]
+        }
+    ]).then( (answer) => {
+        switch (answer.buyMore) {
+            case 'View Products for Sale':
+                
+                break;
+            case 'View Low Inventory':
+                
+                break;
+            case 'Add to Inventory':
+                
+                break;
+            case 'Add New Product':
+                
+                break;
+            case 'Nothing, exit':
+                connection.end();
+                break;
+        }
+    })
 
 
 });
