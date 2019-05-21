edb975eb75a16fe4d6a91f0b64acd618587436f8
Donna Kuang <dolaku@gmail.com>
2019-05-21 08:49:34 -0400

    Differentiated SQL seeds to separate file

--- schema.sql
+++ schema.sql
@@ -1,4 +1,5 @@
 DROP DATABASE IF EXISTS bamazon_db;
+
 CREATE database bamazon_db;
 
 USE bamazon_db;
@@ -10,19 +11,3 @@ CREATE TABLE products (
   price DECIMAL(10,2) NULL,
   stock_quantity INT
 );
-
-INSERT INTO products (product_name,department_name,price,stock_quantity)
-VALUES ('t-shirt','apparel',3.50,80),
-('laptop','electronics',699,25),
-('windshield wipers','auto',18.88,45),
-('water bottle','sports',12.50,55),
-('headphones','electronics',249.99,27),
-('bath towel','bath',8.99,35),
-('sauce pan','kitchen',32.75,14),
-('dish soap','kitchen',3.50,38),
-('zip hoodie','apparel',25.50,66),
-('baking sheet','kitchen',16.99,10);
-
-SELECT * FROM products;
-SELECT item_id AS ID, product_name AS Product, price AS Price, stock_quantity AS 'Qty Available' FROM products;
-
